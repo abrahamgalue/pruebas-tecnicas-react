185b647c174393456cba65e250729b2455932c26
Abraham Galue <abrahamgalue512@gmail.com>
2024-07-06 12:46:18 -0400

    Minor changes in function to update the reading list

---
+++ 02-reading-list/src/utils/updateCart.ts
@@ -0,0 +1,26 @@
+import { localStorageItems, updateLocalStorage } from './localStorage'
+import { BooksState } from '../types'
+
+const { BOOKS } = localStorageItems
+
+export const updateBookInCartState = (
+  state: BooksState,
+  ISBN: string,
+  isInCart: boolean
+) => {
+  const bookIndex = state.findIndex(book => book.ISBN === ISBN)
+
+  if (bookIndex >= 0) {
+    const newState = [
+      ...state.slice(0, bookIndex),
+      { ...state[bookIndex], isInCart },
+      ...state.slice(bookIndex + 1),
+    ]
+
+    updateLocalStorage(BOOKS, newState)
+    return newState
+  }
+
+  updateLocalStorage(BOOKS, state)
+  return state
+}
