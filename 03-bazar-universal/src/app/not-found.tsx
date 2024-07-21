4bb55ef0a8a0a0bfb45a7f8ac661c5379d3b2172
Abraham Galue <abrahamgalue512@gmail.com>
2024-07-21 11:26:45 -0400

    Added 404 error page

--- 03-bazar-universal/src/app/not-found.tsx
+++ 03-bazar-universal/src/app/not-found.tsx
@@ -1,11 +1,14 @@
 import Link from 'next/link'
+import styles from './not-found.module.css'
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href='/'>Return Home</Link>
-    </div>
+    <main className={styles.main}>
+      <h2 className={styles.title}>Not Found</h2>
+      <p className={styles.info}>Could not find requested resource</p>
+      <Link href='/' className={styles.btn}>
+        Return Home
+      </Link>
+    </main>
   )
 }
