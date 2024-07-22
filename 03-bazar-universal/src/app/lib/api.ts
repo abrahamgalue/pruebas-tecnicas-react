9f121c19a353347f541a7de0f1cb3e22f176c1d8
Abraham Galue <abrahamgalue512@gmail.com>
2024-07-22 10:16:09 -0400

    Env added to the project

--- 03-bazar-universal/src/app/lib/api.ts
+++ 03-bazar-universal/src/app/lib/api.ts
@@ -1,12 +1,12 @@
 import { Mock, Product } from '../../../types'
 
+const apiUrl = process.env.API_URL || 'http://localhost:3000/api'
+
 const api = {
   item: {
     search: async (query: string) => {
       const encodedQuery = encodeURIComponent(query)
-      const data = await fetch(
-        `http://localhost:3000/api/items?q=${encodedQuery}`
-      )
+      const data = await fetch(`${apiUrl}/items?q=${encodedQuery}`)
       const res = (await data.json()) as Mock
 
       const { products, limit, skip, total, categories, status } = res
@@ -14,7 +14,7 @@ const api = {
       return { products, limit, skip, total, categories, status }
     },
     fetch: async (id: string) => {
-      const data = await fetch(`http://localhost:3000/api/items/${id}`)
+      const data = await fetch(`${apiUrl}/items/${id}`)
       const res = data.json() as Promise<Product>
 
       return res
